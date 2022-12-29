aaa3e4aecbc9e78381de3426f733d16e3fb52bca
Hans Alemão <aulasparticularesdealemaosp@gmail.com>
2022-12-29 12:13:00 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,11 +7,15 @@ def get_video_len_and_frames(videofile):
         frame_rate = video.get(cv2.CAP_PROP_FPS)
         total_num_frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
         duration = total_num_frames / frame_rate
+        IMG_H = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        IMG_W = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
         return {
             "file_path": videofile,
             "duration": duration,
             "total_frames": total_num_frames,
             "frame_rate": frame_rate,
+            "width": IMG_W,
+            "height": IMG_H,
         }
     except Exception:
         return {
@@ -19,5 +23,6 @@ def get_video_len_and_frames(videofile):
             "duration": None,
             "total_frames": None,
             "frame_rate": None,
+            "width": None,
+            "height": None,
         }
-
